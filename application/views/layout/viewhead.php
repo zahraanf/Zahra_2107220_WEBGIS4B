8a9534f04735f4e9b6e6041b86fcb01538af301b
zahraanf <zahranfauziah@upi.edu>
2023-05-17 12:36:27 +0700

    Modul 2

---
+++ application/views/layout/viewhead.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Top Navigation</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="<?=base_url()?>assets/template/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?=base_url()?>assets/template/dist/css/adminlte.min.css">
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="<?=base_url()?>assets/template/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?=base_url()?>assets/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=base_url()?>assets/template/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?=base_url()?>assets/template/dist/js/demo.js"></script>
+
+</head>
---
+++ application/views/layout/viewunion.php
@@ -0,0 +1,6 @@
+<?php
+require_once('viewhead.php');
+require_once('viewheader.php');
+require_once('viewnav.php');
+require_once('viewcontent.php');
+require_once('viewfooter.php');
